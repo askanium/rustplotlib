bb9dd4fa8e1e2357712c9135aaa6cd844e2d69b7
Iulian Gulea <iulian.gulea@gmail.com>
2020-03-28 09:38:02 +0200

    [feat] refactored chart mod add margins to charts.

--- src/chart.rs
+++ src/chart.rs
@@ -1,28 +1,37 @@
-//! # BarChart
+//! # Chart
 //!
-//! Represents a horizontal or vertical bar chart.
+//! A chart consists of a [View] and between 2 and 4 [Axis]
+//! that define the bounds of the [Dataset]s present in the [View].
 //!
-//! ## Details
-//!
-//! A [BarChart] should have at least one [ScaleBand] or [ScaleOrdinal] axis.
+//! Charts are the smallest self-sufficient entities that can be saved as a file.
 
 use std::ffi::OsStr;
 use std::path::Path;
 use svg;
 use svg::parser::Error;
 use svg::node::element::Group;
-use crate::{view, View, VerticalBarDataset};
-use crate::datasets::Dataset;
-use crate::components::DatumRepresentation;
+use crate::view::View;
 
 /// The BarChart struct definition.
-pub struct BarChart<'a> {
+pub struct Chart<'a> {
+    margin_top: usize,
+    margin_bottom: usize,
+    margin_right: usize,
+    margin_left: usize,
+    width: usize,
+    height: usize,
     view: &'a View<'a>,
 }
 
-impl<'a> BarChart<'a> {
+impl<'a> Chart<'a> {
     pub fn with_view(view: &'a View<'a>) -> Self {
         Self {
+            margin_top: 20,
+            margin_bottom: 50,
+            margin_right: 20,
+            margin_left: 60,
+            width: 600,
+            height: 400,
             view,
         }
     }
@@ -33,6 +42,8 @@ impl<'a> BarChart<'a> {
 
     pub fn to_svg(&self) -> Result<Group, Error> {
         let mut group = Group::new()
+            .set("transform", format!("translate({},{})", self.margin_left, self.margin_top))
+            .set("class", "g-chart")
             .add(self.view.to_svg()?);
 
         Ok(group)
@@ -45,9 +56,9 @@ impl<'a> BarChart<'a> {
                 match self.to_svg() {
                     Ok(svg_content) => {
                         let document = svg::Document::new()
-                            .set("width", 800)
-                            .set("height", 400)
-                            .set("viewBox", (0, 0, 800, 400))
+                            .set("width", self.width)
+                            .set("height", self.height)
+                            .set("viewBox", (0, 0, self.width, self.height))
                             .add(svg_content);
 
                         svg::save(path, &document).unwrap()
