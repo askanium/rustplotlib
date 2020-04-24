d98aeadbcfa529bb423fdccb6a8b368c0945c8ff
Iulian Gulea <iulian.gulea@gmail.com>
2020-04-24 15:31:51 +0300

    [gallery] add examples of line and area series charts.

---
+++ examples/line_series_chart.rs
@@ -0,0 +1,47 @@
+use charts::{Chart, ScaleLinear, MarkerType, PointLabelPosition, LineSeriesView};
+
+fn main() {
+    // Define chart related sizes.
+    let width = 800;
+    let height = 600;
+    let (top, right, bottom, left) = (90, 40, 50, 60);
+
+    // Create a band scale that will interpolate values in [0, 200] to values in the
+    // [0, availableWidth] range (the width of the chart without the margins).
+    let x = ScaleLinear::new()
+        .set_domain(vec![0_f32, 200_f32])
+        .set_range(vec![0, width - left - right]);
+
+    // Create a linear scale that will interpolate values in [0, 100] range to corresponding
+    // values in [availableHeight, 0] range (the height of the chart without the margins).
+    // The [availableHeight, 0] range is inverted because SVGs coordinate system's origin is
+    // in top left corner, while chart's origin is in bottom left corner, hence we need to invert
+    // the range on Y axis for the chart to display as though its origin is at bottom left.
+    let y = ScaleLinear::new()
+        .set_domain(vec![0_f32, 100_f32])
+        .set_range(vec![height - top - bottom, 0]);
+
+    // You can use your own iterable as data as long as its items implement the `PointDatum` trait.
+    let line_data = vec![(12, 54), (100, 40), (120, 50), (180, 70)];
+
+    // Create Line series view that is going to represent the data.
+    let line_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_marker_type(MarkerType::Circle)
+        .set_label_position(PointLabelPosition::N)
+        .load_data(&line_data).unwrap();
+
+    // Generate and save the chart.
+    Chart::new()
+        .set_width(width)
+        .set_height(height)
+        .set_margins(top, right, bottom, left)
+        .add_title(String::from("Line Chart"))
+        .add_view(&line_view)
+        .add_axis_bottom(&x)
+        .add_axis_left(&y)
+        .add_left_axis_label("Custom Y Axis Label")
+        .add_bottom_axis_label("Custom X Axis Label")
+        .save("line-chart.svg").unwrap();
+}
